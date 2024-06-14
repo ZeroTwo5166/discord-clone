0ea0abe8307a24a9bac8f89974b3c5d3fcd2b0a4
ZeroTwo5166 <subarna.gurung23@gmail.com>
2024-06-14 13:22:26 +0200

    first

---
+++ lib/initial-profile.ts
@@ -0,0 +1,42 @@
+import { auth, currentUser, redirectToSignIn } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+
+
+export const initialProfile = async() => {
+    const user = await currentUser();
+    //const { userId } = auth();
+
+    if(!user){
+        return {
+            redirect: {
+                destination: '/sign-in',
+                permanent:false
+            },
+        };
+    }
+    
+
+    const profile = await db.profile.findUnique({
+        where:{
+            userId : user.id
+        }
+    });
+
+    if(profile){
+        return profile
+    }
+
+    const newProfile = await db.profile.create({
+        data:{
+            userId : user.id,
+            name : `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    });
+
+
+    return newProfile;
+
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains: ["utfs.io", "uploadthing.com"] //use remotePatterns
+    }
+};
 
 export default nextConfig;
